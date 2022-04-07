f1aba308dcf58decac59cb632396956026211aff
patrick yard <patrick.yard@bristol.ac.uk>
2022-04-07 11:41:49 +0100

    waveguide was in wrong folder whoops

---
+++ wavguide.py
@@ -0,0 +1,36 @@
+import numpy as np
+import os
+import sys
+from scipy.constants import c
+from mode_solutions import Solver
+
+GENERAL_SIM_PARAMS = {}
+
+class Waveguide(Solver):
+
+    def __init__(self,waveguide_parameters:dict, general_params = GENERAL_SIM_PARAMS mode = 'TE',hide = True,close = True):
+
+        '''Class representing a Waveguide component. Inputs waveguide parameters and simulation parameters'''
+
+        self.waveguide_parameters = waveguide_parameters
+        self.waveguide_parameters.update(general_params)
+
+        super.__init__(self.waveguide_parameters)
+    
+    def get_field_profile(self,index:int or list) -> dict():
+
+        '''Function to get field profile for specificed mode index or indices'''
+
+        return
+
+    def get_dispersion_information(self) -> dict():
+
+        '''Function to return simulated n_eff vs f'''
+
+        return
+
+    def parameter_sweep(self,param_dict:dict)-> dict:
+
+        '''Function to sweep a given waveguide parameter'''
+
+        return
